a0ab082b4227c2497cd54bc44d3c90286b26edb0
gnd <gnd@itchybit.org>
2018-08-07 23:11:11 +0200

    meh

--- db_export.sh
+++ db_export.sh
@@ -48,6 +48,8 @@ do
 	chmod 600 $LOCAL_DIR/$DB_NAME.sql
 
 	# dump user:pass
+    # this is good when migrating and not wanting to change all zillion user passwords (which in itself is a good thing, but takes a lot of time)
+    # on import we just insert the user & hashed pw into the new sql instance
 	echo "Exporting $DB_NAME credentials .."
 	if [[ $DUMP_USERS -eq "1" ]]; then
 		USER_NAME=`mysql -u $DB_USER -h $DB_HOST -p$DB_PASS -e "use mysql; select User from db where Db = '$DB_NAME'"|tail -1`
