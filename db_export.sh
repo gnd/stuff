9a5a74be402dd5a0c1cbe113bd747d83d0094d4b
gnd <gnd@itchybit.org>
2018-08-07 23:32:58 +0200

    meh

--- db_export.sh
+++ db_export.sh
@@ -43,18 +43,18 @@ fi
 # dump single databases
 for DB_NAME in `mysql -u $DB_USER -h $DB_HOST -p$DB_PASS -e "show databases"|grep -v "+"|grep -v "Database"|grep -v information_schema|grep -v performance_schema`
 do
-	echo "Dumping $DB_NAME .."
-	mysqldump --skip-extended-insert --skip-set-charset -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME --result-file=$LOCAL_DIR/$DB_NAME.sql
-	chmod 600 $LOCAL_DIR/$DB_NAME.sql
+    echo "Dumping $DB_NAME .."
+    mysqldump --skip-extended-insert --skip-set-charset -u $DB_USER -h $DB_HOST -p$DB_PASS $DB_NAME --result-file=$LOCAL_DIR/$DB_NAME.sql
+    chmod 600 $LOCAL_DIR/$DB_NAME.sql
 
-	# dump user:pass
+    # dump user:pass
     # this is good when migrating and not wanting to change all zillion user passwords (which in itself is a good thing, but takes a lot of time)
     # on import we just insert the user & hashed pw into the new sql instance
-	echo "Exporting $DB_NAME credentials .."
-	if [[ $DUMP_USERS -eq "1" ]]; then
-		USER_NAME=`mysql -u $DB_USER -h $DB_HOST -p$DB_PASS -e "use mysql; select User from db where Db = '$DB_NAME'"|tail -1`
-		USER_PASS=`mysql -u $DB_USER -h $DB_HOST -p$DB_PASS -e "use mysql; select Password from user where User = '$USER_NAME'"|tail -1`
-		echo "$USER_NAME $USER_PASS" > $LOCAL_DIR/$DB_NAME.txt
-		chmod 600 $LOCAL_DIR/$DB_NAME.txt
-	fi
+    echo "Exporting $DB_NAME credentials .."
+    if [[ $DUMP_USERS -eq "1" ]]; then
+            USER_NAME=`mysql -u $DB_USER -h $DB_HOST -p$DB_PASS -e "use mysql; select User from db where Db = '$DB_NAME'"|tail -1`
+            USER_PASS=`mysql -u $DB_USER -h $DB_HOST -p$DB_PASS -e "use mysql; select Password from user where User = '$USER_NAME'"|tail -1`
+            echo "$USER_NAME $USER_PASS" > $LOCAL_DIR/$DB_NAME.txt
+            chmod 600 $LOCAL_DIR/$DB_NAME.txt
+    fi
 done
