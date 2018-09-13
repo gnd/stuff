4b4575af1167bf0068a45467c95785061e5dfa88
gnd <gnd@itchybit.org>
2018-09-13 14:03:58 +0200

    meh

---
+++ wp_new.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# script to install wordpress
+#
+# Usage:
+#
+# run from the wordpress root dir
+#
+#
+# gnd, 2015 - 2018
+#############################################
+
+# Check if this is run as root
+ROOT=`whoami`
+if [[ $ROOT != "root" ]]; then
+    echo "Please run as root"
+    exit
+fi
+
+# Set some globals
+DATUM=`/bin/date +%D|sed 's/\//_/g'`
+USER=`ls -la .|tail -1|awk {'print $3;'}`
+GROUP="www-data"
+
+# Extract the latest version
+wget https://wordpress.org/latest.tar.gz
+tar -xzvf latest.tar.gz
+cp -pr wordpress/* .
+
+# Set permissions
+echo "Setting ownership to $USER:$GROUP .."
+chown $USER:$GROUP * -R
+chmod 750 * -R
+chmod 770 wp-content -R
+chmod 750 wp-content/themes -R
+chmod 750 wp-content/plugins -R
+echo "done."
+
+# Finish
+echo "Cleaning up.."
+rm -rf wordpress
+rm latest.tar.gz
+echo "done."
+
+echo "Now access the admin and finish the install"
