58667bed9c61f27e1f73f94e4b946b60c0d5c21e
gnd <gnd@itchybit.org>
2019-08-20 15:28:16 +0200

    small edits

--- gc_transcribe.py
+++ gc_transcribe.py
@@ -1,49 +1,33 @@
 #!/usr/bin/env python
-
-# Copyright 2017 Google Inc. All Rights Reserved.
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# taken from:
+#   https://github.com/GoogleCloudPlatform/python-docs-samples/blob/master/speech/cloud-client/transcribe_async.py
 
 """Google Cloud Speech API sample application using the REST API for async
 batch processing.
-Example usage:
-    python transcribe_async.py resources/audio.raw
-    python transcribe_async.py gs://cloud-samples-tests/speech/vr.flac
-"""
 
-""" Usage:
-    - create a account on Google Cloud & create first app
-    - get auth .json like here: https://cloud.google.com/docs/authentication/getting-started
-    - export GOOGLE_APPLICATION_CREDENTIALS
-    - create a new bucket, or use an existing one and upload file to be transcribed (has to be mono)
-    - update bucket permissions (make urself be in Storage Legacy Bucket Owner)
-    - get link of file
-    - run like above
+Usage:
+    1. create a account on Google Cloud & create first app
+    2. get auth .json like here: https://cloud.google.com/docs/authentication/getting-started
+    3. export GOOGLE_APPLICATION_CREDENTIALS=/path/to/json
+    4. create a new bucket, or use an existing one and upload file to be transcribed (has to be mono)
+    5. update bucket permissions (make urself be in Storage Legacy Bucket Owner)
+    6. get link of file (eg. gs://myfile.wav)
+    7. run like:
+            python gc_transcribe.py audio.raw
+            python gc_transcribe.py gs://cloud-samples-tests/speech/vr.flac
 """
 
 import argparse
 import io
 
-
-# [START speech_transcribe_async]
+"""Transcribe the given audio file asynchronously."""
 def transcribe_file(speech_file, lang):
-    """Transcribe the given audio file asynchronously."""
     from google.cloud import speech
     from google.cloud.speech import enums
     from google.cloud.speech import types
     client = speech.SpeechClient()
 
-    # [START speech_python_migration_async_request]
     with io.open(speech_file, 'rb') as audio_file:
         content = audio_file.read()
 
@@ -53,26 +37,16 @@ def transcribe_file(speech_file, lang):
         sample_rate_hertz=48000,
         language_code='en-US')
 
-    # [START speech_python_migration_async_response]
     operation = client.long_running_recognize(config, audio)
-    # [END speech_python_migration_async_request]
 
     print('Waiting for operation to complete...')
     response = operation.result(timeout=2500)
-
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
     for result in response.results:
-        # The first alternative is the most likely one for this portion.
         print(u'Transcript: {}'.format(result.alternatives[0].transcript))
-        print('Confidence: {}'.format(result.alternatives[0].confidence))
-    # [END speech_python_migration_async_response]
-# [END speech_transcribe_async]
+        #print('Confidence: {}'.format(result.alternatives[0].confidence))
 
-
-# [START speech_transcribe_async_gcs]
+"""Asynchronously transcribes the audio file specified by the gcs_uri."""
 def transcribe_gcs(gcs_uri, lang):
-    """Asynchronously transcribes the audio file specified by the gcs_uri."""
     from google.cloud import speech
     from google.cloud.speech import enums
     from google.cloud.speech import types
@@ -88,11 +62,7 @@ def transcribe_gcs(gcs_uri, lang):
 
     print('Waiting for operation to complete...')
     response = operation.result(timeout=2500)
-
-    # Each result is for a consecutive portion of the audio. Iterate through
-    # them to get the transcripts for the entire audio file.
     for result in response.results:
-        # The first alternative is the most likely one for this portion.
         print(u'{}'.format(result.alternatives[0].transcript))
         #print('Confidence: {}'.format(result.alternatives[0].confidence))
 
